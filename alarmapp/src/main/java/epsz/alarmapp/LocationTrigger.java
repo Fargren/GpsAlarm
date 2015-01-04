48a12970804f6198926bb32d868b827d887f32e1
Martin epsz <epsz@technorides.com>
2015-01-03 22:12:50 -0300

    Functional location trigger
    The LocationTrigger now only matches alarms and updates that are close
    to each other

--- alarmapp/src/main/java/epsz/alarmapp/LocationTrigger.java
+++ alarmapp/src/main/java/epsz/alarmapp/LocationTrigger.java
@@ -1,11 +1,26 @@
 package epsz.alarmapp;
 
 public class LocationTrigger extends Trigger {
+    private final double latitude;
+    private final double longitude;
+    private final double radius;
+
     public LocationTrigger(double latitude, double longitude, double radius) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
     }
 
     @Override
     public boolean matches(Trigger trigger) {
-        return true;
+        if (trigger instanceof LocationTrigger) {
+            LocationTrigger locationTrigger = (LocationTrigger) trigger;
+            double yDistance = this.latitude - locationTrigger.latitude;
+            double xDistance = this.longitude - locationTrigger.longitude;
+            double distanceSquared = xDistance * xDistance + yDistance * yDistance;
+            double maxDistance = this.radius + locationTrigger.radius;
+            return distanceSquared < maxDistance * maxDistance;
+        }
+        return false;
     }
 }
